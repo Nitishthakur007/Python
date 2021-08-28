a3e63b0c0b8e1a8d6b9461de2fff3a3a653db10f
Nitishthakur <nitishthakur92@gmail.com>
2021-08-29 00:27:05 +0530

    subtraction function v1.1 added

--- sub.py
+++ sub.py
@@ -1,5 +1,10 @@
-print ( "Subtraction two numbers program ")
+print("Subtraction two numbers program ")
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = a - b
 print(c)
+
+
+# Function for subtraction
+def sub(num1, num2):
+    return num1 - num2
