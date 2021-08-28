58261878d16976837fef0df60fd6473a6f33941f
Nitishthakur <nitishthakur92@gmail.com>
2021-08-29 00:33:18 +0530

    Issue resolved addition function added

--- Add.py
+++ Add.py
@@ -1,6 +1,10 @@
-
-print ( "Adding two numbers program ")
+print("Adding two numbers program ")
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = a + b
 print(c)
+
+
+# Addition Function
+def add(num1, num2):
+    return num1 - num2
